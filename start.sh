22b5567134fd3c0ff3b80cd8d6642f1472f99b9a
mrd <duongtt.dev.mrd@gmail.com>
2022-09-09 21:38:26 +0700

    deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -jar /target/ProjectByMe-0.0.1-SNAPSHOT.jar  > /tmp/website.log 2>&1 &
+nohup java -jar target/ProjectByMe-0.0.1-SNAPSHOT.jar  > website.log 2>&1 &
 echo "Application started successfully"
