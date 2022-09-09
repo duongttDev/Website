16279e47efd90dca632f171ac66afebab1b7d242
mrd <duongtt.dev.mrd@gmail.com>
2022-09-09 21:44:29 +0700

    build

--- stop.sh
+++ stop.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-proccess_id=$( ps aux | grep frontend | grep 'ProjectByMe-0.0.1-SNAPSHOT' | awk '{print $2}')
-if test -n "$proccess_id"; then
-    echo "Attempting to kill $proccess_id..."
-    kill -9 $proccess_id
-        echo "Process $proccess_id terminated"
+PID=$(ps -ef | grep ProjectByMe-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{ print $2 }')
+if [ -z "$PID" ]
+then
+    echo "Application is already stopped"
 else
-    echo "Application is not running"
-fi;
+    echo "stopping application"
+    echo kill $PID
+    kill $PID
+fi
