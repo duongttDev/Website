5273289678a54e42a23f9dec385d79dc61693064
mrd <duongtt.dev.mrd@gmail.com>
2022-09-09 22:19:06 +0700

    up flie

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ nohup java -jar target/ProjectByMe-0.0.1-SNAPSHOT.jar  > target/website.log 2>&1
 PID=$(ps -ef | grep ProjectByMe-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{ print $2 }')
 if [ -z "$PID" ]
 then
-    echo "Application started successfully"
-else
     echo "Application started fail"
+else
+    echo "Application started successfully"
 fi
 
